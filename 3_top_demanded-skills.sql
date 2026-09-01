1d15dbd6375001e6a634abca35d3273e80a183b2
Obonganwan Akpabio <79636651+queenieakpabio@users.noreply.github.com>
2026-09-01 13:10:23 +0100

    Add files via upload

---
+++ 3_top_demanded-skills.sql
@@ -0,0 +1,9 @@
+
+
+SELECT *
+From job_postings_fact 
+INNER JOIN
+    skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN
+    skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+LIMIT 5
